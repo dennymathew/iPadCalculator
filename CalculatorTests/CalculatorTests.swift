39a434ee7d54864a0d03287e87d514d55f019d48
Denny Mathew <devdenmathew@gmail.com>
2023-02-05 19:49:20 +0100

    Adds unit tests

--- CalculatorTests/CalculatorTests.swift
+++ CalculatorTests/CalculatorTests.swift
@@ -18,12 +18,17 @@ final class CalculatorTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testCalculatedResultForAddition() {
+        var calc = Calculator()
+        calc.onPressDigit(5)
+        calc.onPressDot()
+        calc.onPressDigit(7)
+        calc.onPressOperator(.plus)
+        calc.onPressDigit(4)
+        calc.onPressDot()
+        calc.onPressDigit(3)
+        calc.onCalculate()
+        XCTAssertEqual(calc.displayedValue, "10")
     }
 
     func testPerformanceExample() throws {
