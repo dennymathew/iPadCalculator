c87463ebac3589801158bbe8603b0ba8d79ad9ac
Denny Mathew <devdenmathew@gmail.com>
2023-02-11 23:02:53 +0100

    Updates danger file

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,39 +1,27 @@
 import Danger
+import Foundation
 
 let danger = Danger()
+let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 
-// Pull request size
-let bigPRThreshold = 500
-let additions = danger.github.pullRequest.additions!
-let deletions = danger.github.pullRequest.deletions!
-let changedFiles = danger.github.pullRequest.changedFiles!
-if (additions + deletions > bigPRThreshold) {
-    warn("PR size seems relatively large. ✂️ If this PR contains multiple changes, please split each into separate PR will helps faster, easier review.")
+if editedFiles.count - danger.git.deletedFiles.count > 300 {
+  warn("Big PR, try to keep changes smaller if you can")
 }
 
-// Pull request body validation
-if danger.github.pullRequest.body == nil || danger.github.pullRequest.body!.isEmpty {
-    warn("PR has no description. 📝 You should provide a description of the changes that have made.")
+// Encourage writing up some reasoning about the PR, rather than just leaving a title.
+let body = danger.github.pullRequest.body?.count ?? 0
+let linesOfCode = danger.github.pullRequest.additions ?? 0
+if body < 3 && linesOfCode > 10 {
+    warn("Please provide a summary in the Pull Request description")
 }
 
-// Pull request title validation
-let prTitle = danger.github.pullRequest.title
-if prTitle.contains("WIP") {
-    warn("PR is classed as _Work in Progress_.")
-}
-if prTitle.count < 5 {
-    warn("PR title is too short. 🙏 Please use this format `[SDK-000] Your feature title` and replace `000` with Jira task number.")
-}
-if !prTitle.contains("[SDK-") {
-    warn("PR title does not containe the related Jira task. 🙏 Please use this format `[SDK-000] Your feature title` and replace `000` with Jira task number.")
+// Support running via `danger local`
+if danger.github != nil {
+    // These checks only happen on a PR
+    if danger.github.pullRequest.title.contains("WIP") {
+        warn("PR is classed as Work in Progress")
+    }
 }
 
-// Files changed and created should includes unit tests
-let modified = danger.git.modifiedFiles
-let editedFiles = modified + danger.git.createdFiles
-let testFiles = editedFiles.filter { ($0.contains("Tests") || $0.contains("Test")) && ($0.fileType == .swift  || $0.fileType == .m) }
-if testFiles.isEmpty {
-    warn("PR does not contain any files related to unit tests ✅ (ignore if your changes do not require tests)")
-}
-
-message("🎉 The PR added \(additions) and removed \(deletions) lines. 🗂 \(changedFiles) files changed.")
+print("Running Swiftlint on changed files...")
+SwiftLint.lint(.files(editedFiles), inline: true, strict: true, quiet: false)
