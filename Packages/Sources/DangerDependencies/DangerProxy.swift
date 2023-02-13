b322bf3de86c2d82e009fe030747ae0d129168af
Denny Mathew <devdenmathew@gmail.com>
2023-02-13 16:48:46 +0100

    Updates danger file path and xcresults path in danger file

--- Packages/Sources/DangerDependencies/DangerProxy.swift
+++ Packages/Sources/DangerDependencies/DangerProxy.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+// SCRIPT TO ADD TO BITRISE
+//#!/usr/bin/env bash
+//# fail if any commands fails
+//set -e
+//echo "################### DANGER ######################"
+//echo "Install Danger"
+//brew install danger/tap/danger-swift
+//echo "Run danger"
+//danger-swift ci
+//echo "#################################################"
